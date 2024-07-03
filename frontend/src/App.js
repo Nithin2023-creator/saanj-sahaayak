4498bb3bff2085b54f6368b2b6b9d9574c576844
Nithin Chowdary <nithinchowdary2354@gmail.com>
2024-07-03 15:10:55 +0530

    intial

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import {Link, Route, Routes} from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import './App.css';
+import Navbar from './components/Navbar';
+
+
+function App() {
+  return (
+    <Navbar/>
+    
+  );
+}
+
+export default App;
